36e9d93459caf1c22b0a77ad988ca913279bd51f
kittswaye <153619900+kittswaye@users.noreply.github.com>
2024-06-06 22:26:56 +0300

    test

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,20 +1,21 @@
-export default function searchPicture(keyword) {
+import axios from 'axios';
+
+
+export default async function searchPicture(keyword, page=1, per_page=15) {
     const API_KEY = '44204846-4ec433e2c1cc73895dee1a984';
     const API_URL = 'https://pixabay.com/api/';
-    const searchParams = new URLSearchParams({
-        key: API_KEY,
-        q: keyword,
-        image_type: 'photo',
-        orientation: 'horizontal',
-        safesearch: true
+
+    const response = await axios.get(API_URL, {
+        params: {
+            key: API_KEY,
+            q: keyword,
+            image_type: 'photo',
+            orientation: 'horizontal',
+            safesearch: true,
+            per_page: per_page,
+            page: page
+        }
     });
 
-    return fetch(`${API_URL}?${searchParams}`
-    )
-    .then((response) => {
-        if (!response.ok) {
-            throw new Error(response.status);
-        }
-        return response.json();
-    });
+    return response.data;
 }
--- src/main.js
+++ src/main.js
@@ -5,6 +5,11 @@ import searchPicture from "./js/pixabay-api";
 import { renderPictures, clearGallery } from "./js/render-functions";
 
 
+const errorMessage = 'Sorry, there are no images matching your search query. Please try again!';
+let page = 1;
+let per_page = 15;
+let searchQuery = '';
+
 function showError(message) {
     iziToast.error({
         title: 'Error',
@@ -13,24 +18,60 @@ function showError(message) {
     });
 }
 
-document.getElementById('search-form').addEventListener('submit', function (event) {
-    const errorMessage = 'Sorry, there are no images matching your search query. Please try again!';
+function hideElement(id) {
+    document.getElementById(id).style.display = 'none';
+}
+
+function showElement(id) {
+    document.getElementById(id).style.display = 'block';
+}
+
+document.getElementById('search-form').addEventListener('submit', async (event) => {
     event.preventDefault();
-    const searchQuery = document.getElementById('pixabay').value.trim();
+    hideElement('loadmore');
+    searchQuery = document.getElementById('pixabay').value.trim();
+    page = 1;
+
     if (searchQuery === '') {
         showError(errorMessage);
     } else {
-        document.getElementById('loading').style.display = 'block';
+        showElement('loading');
         clearGallery();
-        searchPicture(searchQuery)
-            .then((pictures) => {
-                if (pictures.total !== 0) {
-                    renderPictures(pictures);
-                } else {
-                    showError(errorMessage);
-                }
-                document.getElementById('loading').style.display = 'none';
-            })
-            .catch((error) => showError('Error'));
+        try {
+            const pictures = await searchPicture(searchQuery, page, per_page);
+            if (pictures.total !== 0) {
+                renderPictures(pictures);
+                showElement('loadmore');
+            } else {
+                showError(errorMessage);
+            }
+            hideElement('loading');
+        } catch (error) {
+            showError('Error')
+        }
+    }
+});
+
+document.getElementById('loadmore').addEventListener('click', async () => {
+    document.getElementById('loading').style.display = 'block';
+    page += 1;
+
+    function scroll() {
+        let card = document.querySelector(".gallery-item");
+        let rect = card.getBoundingClientRect();
+        window.scrollBy({ left: 0, top: rect.height * 3.5, behavior: "smooth" });
+    }
+
+    try {
+        const pictures = await searchPicture(searchQuery, page, per_page);
+        renderPictures(pictures);
+        scroll();
+        if (page * per_page >= pictures.totalHits) {
+            hideElement('loadmore');
+            showError("We're sorry, but you've reached the end of search results.");
+        }
+        hideElement('loading');
+    } catch (error) {
+        showError('Error')
     }
 });
